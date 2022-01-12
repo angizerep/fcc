f6abbb08db9e7bea957c3bbf75ce5fb5d9c86862
Angeles <angi.zerep@gmail.com>
2022-01-12 00:22:10 -0400

    done

--- javascript-algorithms/palindrome-checker.js
+++ javascript-algorithms/palindrome-checker.js
@@ -50,96 +50,22 @@ We'll also pass strings with special symbols, such as 2A3*3a2,
 function palindrome(str) {
 
     let areEquals = false;
-    // var stringWithoutDiacritics = removeDiacritics(str);
-    // // console.log(' stringWithoutDiacritics ', stringWithoutDiacritics)
 
-    // var stringWithoutNumbers = stringWithoutDiacritics.replace(/[0-9]/g, '');
-    // // console.log(' stringWithoutNumbers ', stringWithoutNumbers)
+    var newString = str.toLowerCase().replace(/([^a-z0-9])*/g, "").split("");
 
-    // var stringWithoutSpaces = stringWithoutNumbers.replace(/ /g, '');
-    // // console.log(' stringWithoutSpaces ', stringWithoutSpaces)
+    if ( newString.length > 0 ){
+        for ( let i = 0 ; i < Math.ceil(newString.length/2) ; i++ ){
 
-    // var stringLowerCase = stringWithoutSpaces.toLowerCase() 
-    // // console.log(' stringLowerCase ', stringLowerCase)
-
-    // var stringWithoutUnderScore = stringLowerCase.replace(/_/g, '');
-    // // console.log(' stringWithoutUnderScore ', stringWithoutUnderScore)
-
-    // var stringWithoutThat = stringWithoutUnderScore.replace(/-/g, '');
-    // // console.log(' stringWithoutUnderScore ', stringWithoutThat)
-
-    // var stringWithoutSpecialChars = stringWithoutThat.replace(/([.,*+?^=!:${}()|\[\]\/\\])/g, '');
-    // // console.log(' stringWithoutSpecialChars ', stringWithoutSpecialChars)
-
-    // var stringWithoutSpecialChars = stringWithoutSpecialChars.toLowerCase() 
-    var stringWithoutSpecialChars = str.toLowerCase().replace(/([^a-z0-9])*/g, "").split("");
-
-        console.log(' stringWithoutSpecialChars ', stringWithoutSpecialChars)
-
-
-    console.log( 'stringWithoutSpecialChars.length ',stringWithoutSpecialChars.length)
-
-    console.log('Math.ceil(stringWithoutSpecialChars.length/2 ',Math.ceil(stringWithoutSpecialChars.length/2))
-
-    console.log( 'stringWithoutSpecialChars.length ',stringWithoutSpecialChars.length)
-    console.log( 'stringWithoutSpecialChars[k] ',stringWithoutSpecialChars[stringWithoutSpecialChars.length - 1])
-
-    if ( stringWithoutSpecialChars.length > 0 ){
-        for ( let i = 0 ; i < Math.ceil(stringWithoutSpecialChars.length/2) ; i++ ){
-            // for ( let k = (stringWithoutSpecialChars.length - 1 );  k >= Math.ceil(stringWithoutSpecialChars.length/2) ; k--){
-    
-                console.log(' iiiiii ', i )
-                // console.log(' kkkkkk ', k )
-    
-                console.log(' stringLowerCase[i] ', stringWithoutSpecialChars[i] )
-                // console.log(' stringLowerCase[k] ', stringWithoutSpecialChars[k] )
-    
-                console.log(' stringLowerCase NO SE ', stringWithoutSpecialChars[stringWithoutSpecialChars.length - i - 1] )
-    
-        
-                if ( ( stringWithoutSpecialChars[i] === stringWithoutSpecialChars[stringWithoutSpecialChars.length - i - 1] ) ){
-                    console.log('IF')
+                if ( ( newString[i] === newString[newString.length - i - 1] ) ){
                     areEquals = true
                 }
                 else {
-                    console.log('ELSE')
                     areEquals = false
                     break;
                 }
-            // }
         }
-    }else {
-        areEquals = true
     }
 
 
-    console.log(' areEquals ',areEquals)
     return areEquals;
 }
-
-function removeDiacritics(string){
-
-    var diacritics = [
-        {char: 'A', base: /[\300-\306]/g},
-        {char: 'a', base: /[\340-\346]/g},
-        {char: 'E', base: /[\310-\313]/g},
-        {char: 'e', base: /[\350-\353]/g},
-        {char: 'I', base: /[\314-\317]/g},
-        {char: 'i', base: /[\354-\357]/g},
-        {char: 'O', base: /[\322-\330]/g},
-        {char: 'o', base: /[\362-\370]/g},
-        {char: 'U', base: /[\331-\334]/g},
-        {char: 'u', base: /[\371-\374]/g},
-        {char: 'N', base: /[\321]/g},
-        {char: 'n', base: /[\361]/g},
-        {char: 'C', base: /[\307]/g},
-        {char: 'c', base: /[\347]/g}
-    ]
-
-    diacritics.forEach(function(letter){
-        string = string.replace(letter.base, letter.char);
-    });
-
-    return string;
-};
-
