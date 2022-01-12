dc347ee21465b8d4ee8f3f5d265d1fe617c9274a
Angeles <angi.zerep@gmail.com>
2022-01-12 00:26:47 -0400

    new

---
+++ javascript-algorithms/roman-numeral-converter.js
@@ -0,0 +1,9 @@
+/*
+Convert the given number into a roman numeral.
+
+All roman numerals answers should be provided in upper-case.
+
+    I, V, X,  L,   C,   D, and    M, standing respectively for 
+    1, 5, 10, 50, 100, 500, and 1,000.
+
+*/
