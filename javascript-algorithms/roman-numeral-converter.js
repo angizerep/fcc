2e9aa56794e248c5ff20444cb847b950be0a17a8
Angeles <angi.zerep@gmail.com>
2022-01-27 22:11:59 -0400

    roman

--- javascript-algorithms/roman-numeral-converter.js
+++ javascript-algorithms/roman-numeral-converter.js
@@ -7,3 +7,14 @@ All roman numerals answers should be provided in upper-case.
     1, 5, 10, 50, 100, 500, and 1,000.
 
 */
+
+function convertToRoman(num) {
+
+    var unidad = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    var decena = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    var centena = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    var mil = ["", "M", "MM", "MMM", "MMMM"];
+
+
+    return roman;
+}
